0f0dfa7eda71c05548cbf4e765032a51d606adea
hartsean <hartsean@gmail.com>
2020-10-26 18:36:10 -0500

    (feat) install axios and get data from trivia-data.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Trivia from './components/trivia-main'
+import axios from 'axios'
+import React, {setState} from 'react';
 
+//this.setState({content: response.data}))
 function App() {
+  const getRealData = () => {
+    axios.get('trivia-data/trivia-data.json', { headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     }})
+    .then(response => console.log(response) )
+    .catch(error => console.log('err:', error))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div class="trivia-container">
-        hello!
+      <div className="trivia-container">
+        <button onClick={getRealData}>Get Data</button>
+        <Trivia getRealData={getRealData}/>
       </div>
     </div>
   );
