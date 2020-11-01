c83dea8074ce0c1540fd73885f9848c1ff7b1293
hartsean <hartsean@gmail.com>
2020-11-01 11:03:40 -0800

    (feat) create answer validator and update points on click

--- src/components/trivia-main.js
+++ src/components/trivia-main.js
@@ -3,10 +3,30 @@ import axios from 'axios'
 import React from 'react'
 
 export default class Trivia extends React.Component{
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
   state = {
-    trivia: []
+    trivia: [],
+    points: 1,
+  }
+  checkAnswer(e){
+    e.stopPropagation()
+    // console.log(e.target.className); 
+    if(e.target.className === "correct"){
+      this.setState(prevState => {
+        return {points: prevState.points + 1}
+     })
+      console.log('selected correct');
+    } else {
+      console.log('selected incorrect');
+    }
   }
 
+  handleClick(e){
+    this.checkAnswer(e)
+  }
   componentDidMount() {
     axios.get(`/trivia-data/trivia-data.json`)
       .then(res => {
@@ -15,18 +35,20 @@ export default class Trivia extends React.Component{
       })
     ;
   }
-
   render() {
     return (
       <ul>
-      <Ranking />
+      <Ranking/>
+      Points:{this.state.points}
         { this.state.trivia.map(prompt => 
-          <div>
+          <div key={prompt.question}>
             <div className="question">
               Question: {prompt.question}
             </div>
             <div className="answer">
-              Answer: {prompt.incorrect} {prompt.correct}
+              Answer: 
+              <div className="incorrect" onClick={this.handleClick}>{prompt.incorrect}</div>
+              <div className="correct" onClick={this.handleClick}>{prompt.correct}</div>
             </div>
           </div>
         ) }
